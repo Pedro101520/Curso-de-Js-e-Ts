916d98e7babf1dfefd2dbd997a9923835ea42c27
Pedro101520 <pedrolimazoi10@gmail.com>
2024-02-21 09:37:29 -0300

    Atualização Aula 72

--- 72/index.js
+++ 72/index.js
@@ -7,7 +7,8 @@ function Conta(agencia, conta, saldo){
 
 Conta.prototype.sacar = function(valor){
     //Se o valor for maior que o saldo
-    if(this.saldo < valor){
+    if(valor > this.saldo){
+        console.log(`Saldo insuficiente: ${this.saldo}`);
         this.verSaldo();
         return;
     }
@@ -28,3 +29,27 @@ Conta.prototype.verSaldo = function(){
 const conta1 = new Conta(11, 22, 10000);
 conta1.depositar(11);
 conta1.verSaldo();
+
+//Aqui é a herança, e vai ser demonstrado o polimorfismo - CC = Conta Corrente
+function CC(agencia, conta, saldo, limite){
+    Conta.call(agencia, conta, saldo);
+    this.limite = limite;
+}
+
+//Lembrando, que tem que linkar o método com a classe que está herdando
+CC.prototype = Object.create(Conta.prototype);
+CC.prototype.constructor = CC;
+
+//Aqui já é o polimorfismo
+CC.prototype.sacar = function(valor){
+    //Se o valor for maior que o saldo
+    if(valor > (this.saldo + this.limite)){
+        console.log(`Saldo insuficiente: ${this.saldo}`);
+        return;
+    }
+    this.saldo -= valor;
+};
+
+const contaCorrente = new CC(11, 22, 0, 100);
+contaCorrente.depositar(10);
+console.log(contaCorrente.verSaldo());
